b70544fa7619658946fd9bf3cbf27efd0e5f8998
cammylim <64000790+cammylim@users.noreply.github.com>
2020-08-13 00:27:40 +0800

    submission.

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,10 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        VideoView video = findViewById(R.id.video);
+        video.setVideoURI(Uri.parse("android.resource://"+getPackageName() + "/" + R.raw.rabbid));
+        video.start();
     }
 }
